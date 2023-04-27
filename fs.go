ae18800bd9d69ca42bfa1d9fd015e49a8fc3e3e5
= <=>
2023-04-27 14:20:18 +0000

    Done

--- fs.go
+++ fs.go
@@ -8,10 +8,10 @@ func fileSystem() []string {
 	if len(os.Args) < 3 { //---			---> Default Banner
 		return fileScan("standard.txt")
 	} else if len(os.Args) > 3 { //---			---> Should be: "go run . [STRING] [BANNER]"
-		os.Stderr.WriteString("\033[31mERROR --> \033[30mToo Many Arguments\033[0m")
+		os.Stderr.WriteString("\033[31mERROR --> \033[30mToo Many Arguments\n\033[32mUsage --> \033[34mgo run . [STRING] [BANNER]\033[0m")
 		os.Exit(0)
 	} else if os.Args[2] != "standard" && os.Args[2] != "shadow" && os.Args[2] != "thinkertoy" { //---> Valid Banner
-		os.Stderr.WriteString("\033[31mERROR --> \033[30mInvalid Banner\033[0m")
+		os.Stderr.WriteString("\033[31mERROR --> \033[30mInvalid [BANNER]\n\033[32mValid --> \033[34mstandard - shadow - thinkertoy\033[0m")
 		os.Exit(0)
 	}
 	return fileScan(os.Args[2] + ".txt") //---> User just need to put Banner Name
