6840baea31cf1320db53324c09ef37ff926497ff
= <=>
2023-04-29 18:03:32 +0000

    Done

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import "os"
 func main() {
 
 	if len(os.Args) > 1 && os.Args[1] != "" {
-		words := Format(os.Args[1]) //---			---> Manage Printability & "\n" &  Emptiness
 		lines := fileSystem()       //---			---> Collect Lines from [BANNER] File (os.Args[2])
+		words := Format(os.Args[1]) //---			---> Manage Printability & "\n" &  Emptiness
 		wordArt(words, lines)       //---			---> Print Letter of Argument from File Lines
 	}
 }
